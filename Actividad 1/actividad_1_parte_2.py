821cc4e6fbfa54777f49357e05f4716bc7ea942f
Laura Vargas <vargasnlaura@gmail.com>
2023-04-21 15:30:58 -0300

    algorithms to approximate the number pi

--- Actividad 1/actividad_1_parte_2.py
+++ Actividad 1/actividad_1_parte_2.py
@@ -1,41 +1,36 @@
 import numpy as np
 
-def mejor_aproximacion(numero1, numero2):
-    """Calcula cuál es el número con menor error"""
-    diferencia = []
-    aprox = 0
-    resta = numero2 - numero1
-    if resta > 0:
-        diferencia.append(resta)
-        menor = diferencia[0]
-        if resta < menor:
-            aprox = resta
-    return aprox
-
+#Le pongo un valor límete a las iteraciones porque sino me queda loop infinito
+#Precondición: max_iteraciones > 0
+max_iteraciones = np.array(range(1000000))
 
 def metodo_1(booleano):
     pi = np.pi
     resultado = 0
-    n = 0
-    while booleano == False:
-        serie = 4 * (((-1) ** n) / ((2 * n) + 1))
-        resultado = resultado + serie
-        """ 
-        Esta parte como está propuesta no funciona porque la primer iteración ya es menor que pi
-        entonces va a recorrer el while 1 vez y me va a quedar es_pi = True y termina la ejecución
+    while booleano:
+        for n in max_iteraciones:
+            serie = 4 * (((-1) ** n) / ((2 * n) + 1))
+            resultado = resultado + serie
+        
+            """  Esta parte como está propuesta no funciona porque la segunda iteración ya es menor que pi
+            entonces va a recorrer el while 2 veces, queda es_pi = True y termina la ejecución"""
 
-        if resultado < pi:
-            booleano = True
-        else:
-            n += 1 """
+            if resultado == pi:
+                booleano = False
+            else:
+                booleano = True
 
         booleano
-    return resultado, n
+    #Pongo para que devuelva también el valor de la aproximación
+    return resultado, n 
 
 def main():
-    #Condición inicial para ambos métodos
-    es_pi = False
+    #Precondición para ambos métodos: 
+    es_pi = True
     
-    print(metodo_1(es_pi))
+    #Devuelve el número calculado y la iteración en la que se alcanzó
+    numero, iteracion = metodo_1(es_pi)
+    #Devuelve el número con menor error
+    print("El número pi es aproximadamente: ", numero, " en la iteración ", iteracion)
 
 main()
---
+++ Actividad 1/vargas.py
@@ -0,0 +1,52 @@
+from math import floor
+import numpy as np
+
+#Datos de entrada
+pi = np.pi
+max_iter = np.array(range(100))
+
+def metodo_1(booleano):
+    i = 0
+    resultado = 0
+    while booleano:
+        serie = 4 * (((-1) ** i) / ((2 * i) + 1))
+        resultado = resultado + serie
+        i += 1
+            
+        if (resultado == pi) or (i == max_iter.size):
+            booleano = False
+        else:
+            booleano = True
+        
+        booleano
+    return resultado, i
+
+def metodo_2(booleano):
+    sum_1 = 0
+    sum_2 = 0
+    for i in max_iter:
+        if i % 2 == 0:
+            continue
+
+        signo = (-1) ** (floor(i / 2))
+        sum_1 = sum_1 + (signo * ((1/2) ** i) / i)
+        sum_2 = sum_2 + (signo * ((1/3) ** i) / i)
+        resultado = (sum_1 + sum_2) * 4
+        
+        i += 1
+        booleano
+    return resultado, i
+    
+def aprox_pi():
+
+    #Dato de Entrada
+    es_pi = True
+    
+    aprox_1, iteracion_1 = metodo_1(es_pi)
+    aprox_2, iteracion_2 = metodo_2(es_pi)
+
+    #Datos de salida
+    print("El número pi (por el método 1) es aproximadamente: ", aprox_1, " en la iteración ", iteracion_1)
+    print("El número pi (por el método 2) es aproximadamente: ", aprox_2, " en la iteración ", iteracion_2)
+
+aprox_pi()
